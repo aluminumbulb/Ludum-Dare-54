0c6995b227e145da3f05809dd6514b188aac2fc5
aluminumbulb <47339849+aluminumbulb@users.noreply.github.com>
2023-10-02 13:24:12 -0700

    finished basic mechanics, deleting cam controls

--- src/scripts/index_sketch.js
+++ src/scripts/index_sketch.js
@@ -6,6 +6,9 @@
 //Setting Special input Keys
 
 //failed attempt at checking, kept for cheap enumeration
+let instructionText = 
+`Welcome`
+
 const reservedKeys = {
 SPACE: 32,
 BACKSPACE: 8,
@@ -25,25 +28,73 @@ let vel = 0;
 let steps = 0;
 const s2v = .1;
 
+//Area Variables
+let border;
+let margin = 10;
+
+//Pickup Variables
+let pickups;
+let totalPickups = 30;
+
+//End Point
+let endPoint;
+const overR = 30
 function setup() {
+  //TO DO: normalize window sizes
   createCanvas(windowWidth, windowHeight);
 
+  //Player Setup
   player = new Sprite();
   player.d = 100;
   player.color = 'pink';
   player.stroke = 'red';
+
+  //Scene Setup
+  border = new Sprite();
+  border.w = 0;
+  border.h = 0;
+  border.collider = 'static';
+  //Left Wall
+  border.addCollider(0-((1/2)*(canvas.w)),0,margin, canvas.h);
+  //Right Wall
+  border.addCollider(0+((1/2)*(canvas.w)),0,margin, canvas.h);
+  //Upper Wall
+  border.addCollider(0,(-(1/2)*(canvas.h)-margin),canvas.w, 0);
+  //Lower Wall
+  border.addCollider(0,(+(1/2)*(canvas.h)+margin),canvas.w, 0);
+
+  //Pickup Objects
+  pickups = new Group();
+  pickups.diameter = 10;
+  pickups.x = () => random(0, canvas.w);
+  pickups.y = () => random(0, canvas.h);
+  pickups.amount = totalPickups; 
+
+  player.overlaps(pickups, gain);
+
+  //End Points
+  endPoint = new Sprite(canvas.w-100,canvas.h/2, overR);
+
+  player.overlaps(endPoint, finish)
+
+  changeText('inst',"your mom");
 }
 
 function draw() {
   background(255); // try removing this line and see what happens!
-
-  fill(0);
-  textSize(24);
-  textAlign(CENTER);
-  text('You can do this', width / 2, height / 2);
 }
 
-//----ACTION DEFINITIONS
+//----DYNAMIC AND OVERLAPS
+function gain(player, pickup){
+  energy += pickupE;
+  pickup.remove();
+}
+
+function finish(){
+  console.log("Press Enter to Finish");
+}
+
+//----MECHANIC DEFINITIONS
 function fling(){
   //player velocity = steps * velocity conversion factor (s2v)
   player.speed += steps*s2v;
@@ -54,6 +105,7 @@ function fling(){
   //fling cost gets deducted from energy
   energy -= flingCost;
   
+  changeText('Energy Total', energy);
   //debug statement
   console.log('Fling, steps: ',steps,' energy: ', energy);
 }
@@ -75,6 +127,11 @@ function undo(){
 function pivot(){
   player.direction += 45;
   console.log('pivot')
+}
+
+function explode(){
+  player.remove();
+  console.log('kaboom')
 }
 
 //--------INPUT HANDLING------------
@@ -99,7 +156,11 @@ function keyPressed(){
 
   //Player Controls
   if(keyCode == reservedKeys.SPACE){
-    fling();
+    if(energy>flingCost){
+      fling();
+    }else{
+      explode();
+    }
   }
 
   if(keyCode == reservedKeys.BACKSPACE){
@@ -108,6 +169,10 @@ function keyPressed(){
 
   if(keyCode == reservedKeys.SHIFT){
     amplify();
+  }
+
+  if(keyCode == reservedKeys.ENTER){
+    finish();
   }
 
   if(keyCode == reservedKeys.PERIOD){
@@ -122,6 +187,14 @@ function keyPressed(){
 function checkAlpha(kC){
   return ((keyCode > 64 && keyCode < 91));
 }
+
+//Instructions
+
+function changeText(id, newText){
+  let myDiv = document.getElementById(id);
+  myDiv.innerText = newText;
+}
+
 
 
   
